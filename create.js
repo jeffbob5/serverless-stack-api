712a28a1128e62d41d04c45fe7d9ca10d44aca9c
jeffbob5 <harryhay@gmail.com>
2021-08-12 17:53:52 -0400

    Adding auth to our Serverless API

--- create.js
+++ create.js
@@ -8,14 +8,14 @@ export const main = handler(async (event, context) => {
     TableName: process.env.tableName,
     Item: {
       // The attributes of the item to be created
-      userId: "123", // The id of the author
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
       createdAt: Date.now(), // Current Unix timestamp
     },
   };
-
+  console.info(params);
   await dynamoDb.put(params);
 
   return params.Item;
